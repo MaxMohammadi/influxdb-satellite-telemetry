8fb8ec353d532cab6e44ee673b810c60a45046a8
Max Mohammadi <maxmohammadi@gmail.com>
2021-05-25 16:24:02 -0700

    working

--- send_data.py
+++ send_data.py
@@ -1,14 +1,29 @@
 from datetime import datetime
-from influxdb import InfluxDBClient
 
-client = InfluxDBClient('localhost', 8086, 'admin', 'Password1', 'mydb')
-client.create_database('mydb')
-client.get_list_database()
-client.switch_database('mydb')
+from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client.client.write_api import SYNCHRONOUS
 
-json_payload = []
-data = {}
+token = "wCm6Lbwn2__gUddkK_sFMYAtNnVfi3ti4cxDmRxLSIQtZeIsWcNkFwq2nj6Q8bPS7mQV3NIQqlVz2tzWVMIfmw=="
+org = "Projects"
+bucket = "Telemetry"
 
-json_payload.append(data)
+client = InfluxDBClient(url="http://localhost:8086", token=token)
 
-client.write_points(json_payload)
+write_api = client.write_api(write_options=SYNCHRONOUS)
+
+data = "mem,host=host1 used_percent=23.43234543"
+write_api.write(bucket, org, data)
+
+point = Point("Telemetry").tag(
+    "host", "host1").field(
+        "used_percent", 23.43234543).time(
+            datetime.utcnow(), WritePrecision.NS)
+
+write_api.write(bucket, org, point)
+
+sequence = ["mem,host=host1 used_percent=23.43234543",
+            "mem,host=host1 available_percent=15.856523"]
+write_api.write(bucket, org, sequence)
+
+# query = f'from(bucket: \\"{bucket}\\") |> range(start: -1h)'
+# tables = client.query_api().query(query, org=org)
