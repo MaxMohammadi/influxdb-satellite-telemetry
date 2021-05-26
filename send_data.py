8849d94534e556b5959f2451aaf06dc4c26786a0
Max Mohammadi <maxmohammadi@gmail.com>
2021-05-26 11:21:19 -0700

    getting there

--- send_data.py
+++ send_data.py
@@ -12,19 +12,28 @@ client = InfluxDBClient(url="http://localhost:8086", token=token)
 write_api = client.write_api(write_options=SYNCHRONOUS)
 
 # change time to the new unix stamps in excel
-point = Point("Telemetry").tag(
-    "host", "host1").field(
-        "longitude", 10000.43234543).time(
-            datetime.utcnow(), WritePrecision.NS)
+point = Point("TelemetryDataStream").tag(
+    "type", "coordinate").field(
+        "longitude", 123454).time(
+            "2021-05-26 12:45:00 PDT")
 
 write_api.write(bucket, org, point)
 
-# sequence = ["mem,host=host1 used_percent=10000.43234543"]
+# sequence = ["TelemetryData,host=host1 used_percent=10001.43234543 1685580300"]
 # write_api.write(bucket, org, sequence)
 
 query = '''
 from(bucket: "Telemetry")
     |> range(start: -1m)
+    |> filter(fn:(r) => r["_measurement"] == "TelemetryDataStream")
+    |> filter(fn: (r) => r["_field"] == "longitude")
 '''
 
-print(client.query_api().query(query, org=org))
+result = client.query_api().query(org=org, query=query)
+
+results = []
+for table in result:
+    for record in table.records:
+        results.append((record.get_value(), record.get_field(), record.get_measurement()))
+
+print(results)
