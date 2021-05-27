cbd75d26a6bf8173c0d5aaa5693e79b0a953593e
Max Mohammadi <maxmohammadi@gmail.com>
2021-05-26 18:51:48 -0700

    query working properly

--- send_data.py
+++ send_data.py
@@ -12,12 +12,19 @@ client = InfluxDBClient(url="http://localhost:8086", token=token)
 write_api = client.write_api(write_options=SYNCHRONOUS)
 
 # change time to the new unix stamps in excel
-point = Point("TelemetryDataStream").tag(
+point1 = Point("TelemetryDataStream2").tag(
     "type", "coordinate").field(
-        "longitude", 123454).time(
-            "2021-05-26 12:45:00 PDT")
+        "longitude", -36.85039056).time(
+            datetime.utcnow(), WritePrecision.NS)
 
-write_api.write(bucket, org, point)
+point2 = Point("TelemetryDataStream2").tag(
+    "type", "coordinate").field(
+        "longitude", -35.8234361).time(
+            datetime.utcnow(), WritePrecision.NS)
+
+write_api.write(bucket, org, (point1, point2))
+
+# use sequences to process large amounts of tuples from csv files
 
 # sequence = ["TelemetryData,host=host1 used_percent=10001.43234543 1685580300"]
 # write_api.write(bucket, org, sequence)
@@ -25,15 +32,14 @@ write_api.write(bucket, org, point)
 query = '''
 from(bucket: "Telemetry")
     |> range(start: -1m)
-    |> filter(fn:(r) => r["_measurement"] == "TelemetryDataStream")
-    |> filter(fn: (r) => r["_field"] == "longitude")
+    |> filter(fn: (r) => r["_measurement"] == "TelemetryDataStream2")
 '''
 
-result = client.query_api().query(org=org, query=query)
+result = client.query_api().query(query, org=org)
 
 results = []
 for table in result:
     for record in table.records:
-        results.append((record.get_value(), record.get_field(), record.get_measurement()))
+        results.append((record.get_value(), record.get_field()))
 
 print(results)
